d01dfd2e4fc902f682fbaf69dfe8ee2190c5b81d
SenorPez <senorpez@senorpez.com>
2022-03-15 00:20:21 -0500

    Refactor for consistency

--- shipdesigner/src/main/java/Ship.java
+++ shipdesigner/src/main/java/Ship.java
@@ -19,7 +19,7 @@ class Ship {
     private static final Logger logger = LogManager.getLogger(Ship.class);
     private static final Map<Integer, String> shipClasses = new HashMap<>();
 
-    public Ship() {
+    Ship() {
         // Initialize ship classes
         shipClasses.put(25, "Gunboat");
         shipClasses.put(51, "Corvette");
@@ -32,7 +32,7 @@ class Ship {
         shipClasses.put(1801, "Titan");
     }
 
-    public String getShipClass() {
+    String getShipClass() {
         return shipClasses.entrySet()
                 .stream()
                 .filter(entry -> entry.getKey() <= this.getHullSize())
@@ -62,71 +62,71 @@ class Ship {
         return Double.valueOf(ceilBuildTime).intValue();
     }
 
-    public String getName() {
+    String getName() {
         return name;
     }
 
-    public Ship setName(String name) {
+    Ship setName(String name) {
         logger.info(String.format("Setting origin to %s", name));
         this.name = name;
         return this;
     }
 
-    public Nation getOrigin() {
+    Nation getOrigin() {
         return origin;
     }
 
-    public Ship setOrigin(Nation origin) {
+    Ship setOrigin(Nation origin) {
         logger.info(String.format("Setting origin to %s", origin.name()));
         this.origin = origin;
         return this;
     }
 
-    public Shape getShape() {
+    Shape getShape() {
         return shape;
     }
 
-    public Ship setShape(Shape shape) {
+    Ship setShape(Shape shape) {
         logger.info(String.format("Setting shape to %s", shape.name()));
         this.shape = shape;
         return this;
     }
 
-    public Integer getHullSize() {
+    Integer getHullSize() {
         return hullSize;
     }
 
-    public Ship setHullSize(Integer hullSize) {
+    Ship setHullSize(Integer hullSize) {
         if (hullSize < 25) logger.error("Hull size under 25; setting to 25");
         logger.info(String.format("Setting hull size to %d", hullSize));
         this.hullSize = Math.max(hullSize, 25);
         return this;
     }
 
-    public SheetFormat getSheetFormat() {
+    SheetFormat getSheetFormat() {
         return sheetFormat;
     }
 
-    public Ship setSheetFormat(SheetFormat sheetFormat) {
+    Ship setSheetFormat(SheetFormat sheetFormat) {
         logger.info(String.format("Setting sheet format to %s", sheetFormat.name()));
         this.sheetFormat = sheetFormat;
         return this;
     }
 
-    public Integer getLaidDown() {
+    Integer getLaidDown() {
         return laidDown;
     }
 
-    public Ship setLaidDown(Integer laidDown) {
+    Ship setLaidDown(Integer laidDown) {
         this.laidDown = laidDown;
         return this;
     }
 
-    public BuildMode getBuildMode() {
+    BuildMode getBuildMode() {
         return buildMode;
     }
 
-    public Ship setBuildMode(BuildMode buildMode) {
+    Ship setBuildMode(BuildMode buildMode) {
         this.buildMode = buildMode;
         return this;
     }
--- shipdesigner/src/test/java/ShipTest.java
+++ shipdesigner/src/test/java/ShipTest.java
@@ -12,80 +12,68 @@ class ShipTest {
 
     @Test
     void fieldName() {
+        Ship instance = new Ship().setName("Ship Name");
         String expectedValue = "Ship Name";
-        Ship instance = new Ship();
-        instance.setName("Ship Name");
-
         assertEquals(expectedValue, instance.getName());
     }
 
     @Test
     void fieldOrigin() {
+        Ship instance = new Ship().setOrigin(Nation.OLYMPIAN_REPUBLIC);
+
         Nation expectedValue = Nation.OLYMPIAN_REPUBLIC;
         String expectedNationName = "Olympian Republic";
-        Ship instance = new Ship();
-        instance.setOrigin(Nation.OLYMPIAN_REPUBLIC);
-
         assertEquals(expectedValue, instance.getOrigin());
         assertEquals(expectedNationName, instance.getOrigin().getNationName());
     }
 
     @Test
     void fieldShape() {
+        Ship instance = new Ship().setShape(Shape.SPHEROID);
+
         Shape expectedValue = Shape.SPHEROID;
         String expectedShapeName = "Spheroid";
-        Ship instance = new Ship();
-        instance.setShape(Shape.SPHEROID);
-
         assertEquals(expectedValue, instance.getShape());
         assertEquals(expectedShapeName, instance.getShape().getShapeName());
     }
 
     @Test
     void fieldHullSize() {
+        Ship instance = new Ship().setHullSize(50);
         Integer expectedValue = 50;
-        Ship instance = new Ship();
-        instance.setHullSize(50);
-
         assertEquals(expectedValue, instance.getHullSize());
     }
 
     @Test
     void fieldHullSize_Minimum() {
+        Ship instance = new Ship().setHullSize(11);
         Integer expectedValue = 25;
-        Ship instance = new Ship();
-        instance.setHullSize(11);
-
         assertEquals(expectedValue, instance.getHullSize());
     }
 
     @Test
     void fieldSheetFormat() {
+        Ship instance = new Ship().setSheetFormat(SheetFormat.FOUR_UP);
+
         SheetFormat expectedValue = SheetFormat.FOUR_UP;
         String expectedFormat = "4-Up";
-        Ship instance = new Ship();
-        instance.setSheetFormat(SheetFormat.FOUR_UP);
-
         assertEquals(expectedValue, instance.getSheetFormat());
         assertEquals(expectedFormat, instance.getSheetFormat().getFormat());
     }
 
     @Test
     void fieldLaidDown() {
+        Ship instance = new Ship().setLaidDown(2259);
         Integer expectedValue = 2259;
-        Ship instance = new Ship();
-        instance.setLaidDown(2259);
-
         assertEquals(expectedValue, instance.getLaidDown());
     }
 
     @Test
     void fieldBuildMode() {
+        Ship instance = new Ship().setBuildMode(BuildMode.QUICK);
+
         BuildMode expectedValue = BuildMode.QUICK;
         String expectedBuildModeName = "Quick and Dirty";
-        Ship instance = new Ship();
-        instance.setBuildMode(BuildMode.QUICK);
-
         assertEquals(expectedValue, instance.getBuildMode());
         assertEquals(expectedBuildModeName, instance.getBuildMode().getBuildModeName());
     }
@@ -93,89 +81,89 @@ class ShipTest {
     @Test
     void getShipClass_Gunboat() {
         int hullSize = getRandomNumber(25, 51);
-        Ship instance = new Ship();
+        Ship instance = new Ship().setHullSize(hullSize);
         String expectedValue = "Gunboat";
-        assertEquals(expectedValue, instance.setHullSize(hullSize).getShipClass());
+        assertEquals(expectedValue, instance.getShipClass());
     }
 
     @Test
     void getShipClass_Corvette() {
         int hullSize = getRandomNumber(51, 151);
-        Ship instance = new Ship();
+        Ship instance = new Ship().setHullSize(hullSize);
         String expectedValue = "Corvette";
-        assertEquals(expectedValue, instance.setHullSize(hullSize).getShipClass());
+        assertEquals(expectedValue, instance.getShipClass());
     }
 
     @Test
     void getShipClass_Frigate() {
         int hullSize = getRandomNumber(151, 251);
-        Ship instance = new Ship();
+        Ship instance = new Ship().setHullSize(hullSize);
         String expectedValue = "Frigate";
-        assertEquals(expectedValue, instance.setHullSize(hullSize).getShipClass());
+        assertEquals(expectedValue, instance.getShipClass());
     }
 
     @Test
     void getShipClass_Light_Cruiser() {
         int hullSize = getRandomNumber(251, 351);
-        Ship instance = new Ship();
+        Ship instance = new Ship().setHullSize(hullSize);
         String expectedValue = "Light Cruiser";
-        assertEquals(expectedValue, instance.setHullSize(hullSize).getShipClass());
+        assertEquals(expectedValue, instance.getShipClass());
     }
 
     @Test
     void getShipClass_Heavy_Cruiser() {
         int hullSize = getRandomNumber(351, 501);
-        Ship instance = new Ship();
+        Ship instance = new Ship().setHullSize(hullSize);
         String expectedValue = "Heavy Cruiser";
-        assertEquals(expectedValue, instance.setHullSize(hullSize).getShipClass());
+        assertEquals(expectedValue, instance.getShipClass());
     }
 
     @Test
     void getShipClass_Battlecruiser() {
         int hullSize = getRandomNumber(501, 701);
-        Ship instance = new Ship();
+        Ship instance = new Ship().setHullSize(hullSize);
         String expectedValue = "Battlecruiser";
-        assertEquals(expectedValue, instance.setHullSize(hullSize).getShipClass());
+        assertEquals(expectedValue, instance.getShipClass());
     }
 
     @Test
     void getShipClass_Battleship() {
         int hullSize = getRandomNumber(701, 1001);
-        Ship instance = new Ship();
+        Ship instance = new Ship().setHullSize(hullSize);
         String expectedValue = "Battleship";
-        assertEquals(expectedValue, instance.setHullSize(hullSize).getShipClass());
+        assertEquals(expectedValue, instance.getShipClass());
     }
 
     @Test
     void getShipClass_Leviathan() {
         int hullSize = getRandomNumber(1001, 1801);
-        Ship instance = new Ship();
+        Ship instance = new Ship().setHullSize(hullSize);
         String expectedValue = "Leviathan";
-        assertEquals(expectedValue, instance.setHullSize(hullSize).getShipClass());
+        assertEquals(expectedValue, instance.getShipClass());
     }
 
     @Test
     void getShipClass_Titan() {
         int hullSize = getRandomNumber(1801, 2500);
-        Ship instance = new Ship();
+        Ship instance = new Ship().setHullSize(hullSize);
         String expectedValue = "Titan";
-        assertEquals(expectedValue, instance.setHullSize(hullSize).getShipClass());
+        assertEquals(expectedValue, instance.getShipClass());
     }
 
     @Test
     void getBuildTime_Standard() {
-        Ship instance = new Ship();
-        instance.setHullSize(275);
-        instance.setBuildMode(BuildMode.STANDARD);
+        Ship instance = new Ship()
+                .setHullSize(275)
+                .setBuildMode(BuildMode.STANDARD);
         Integer expectedValue = 29;
         assertEquals(expectedValue, instance.getBuildTime());
     }
 
     @Test
     void getBuildTime_Quick() {
-        Ship instance = new Ship();
-        instance.setHullSize(125);
-        instance.setBuildMode(BuildMode.QUICK);
+        Ship instance = new Ship()
+                .setHullSize(125)
+                .setBuildMode(BuildMode.QUICK);
         Integer expectedValue = 7;
         assertEquals(expectedValue, instance.getBuildTime());
     }
