720aa08f35f6dd420566e273d2282ec0d66e690d
SenorPez <senorpez@senorpez.com>
2022-03-28 20:36:54 -0500

    Created Hull system

---
+++ shipdesigner/src/main/java/com/senorpez/avt/shipdesigner/systems/Hull.java
@@ -0,0 +1,131 @@
+package com.senorpez.avt.shipdesigner.systems;
+
+import com.senorpez.avt.shipdesigner.Ship;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Hull {
+    private Ship ship;
+    private int shrinkEnhancement;
+    private ProductionLevel productionLevel;
+
+    private final String name = "Hull";
+    private final int spacesPerSystem = 0;
+    private final int basicSpacesUsed = 0;
+    private final int actualSpacesUsed = 0;
+    private final int costPerSpace = 1;
+    private final int crewRequiredPerSpace = 0;
+    private final double maintenanceRate = 0.15d;
+
+    private final List<Double> shrinkCost = new ArrayList<>();
+
+    public Hull(Ship ship, int shrinkEnhancement, ProductionLevel productionLevel) {
+        this.ship = ship;
+        this.shrinkEnhancement = shrinkEnhancement;
+        this.productionLevel = productionLevel;
+
+        shrinkCost.addAll(Stream.iterate(new double[]{1, 1d, 1d + 1 / 10d}, t -> new double[]{t[0] + 1, t[2], t[2] + (t[0] + 1) / 10d * 0.75})
+                .limit(10)
+                .map(t -> t[1])
+                .collect(Collectors.toList()));
+    }
+
+    public Ship getShip() {
+        return ship;
+    }
+
+    public Hull setShip(Ship ship) {
+        this.ship = ship;
+        return this;
+    }
+
+    public int getShrinkEnhancement() {
+        return shrinkEnhancement;
+    }
+
+    public Hull setShrinkEnhancement(int shrinkEnhancement) {
+        this.shrinkEnhancement = shrinkEnhancement;
+        return this;
+    }
+
+    public ProductionLevel getProductionLevel() {
+        return productionLevel;
+    }
+
+    public Hull setProductionLevel(ProductionLevel productionLevel) {
+        this.productionLevel = productionLevel;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getSpacesPerSystem() {
+        return spacesPerSystem;
+    }
+
+    public int getBasicSpacesUsed() {
+        return basicSpacesUsed;
+    }
+
+    public int getActualSpacesUsed() {
+        return actualSpacesUsed;
+    }
+
+    public int getCostPerSpace() {
+        return costPerSpace;
+    }
+
+    public int getCrewRequiredPerSpace() {
+        return crewRequiredPerSpace;
+    }
+
+    public double getMaintenanceRate() {
+        return maintenanceRate;
+    }
+
+    final int getBaseCost() {
+        return Double.valueOf(Math.ceil(costPerSpace * ship.getHullSize())).intValue();
+    }
+
+    final int getEnhancedCost() {
+        return Double.valueOf(Math.ceil(getBaseCost() * shrinkCost.get(shrinkEnhancement))).intValue();
+    }
+
+    final int getCrewRequired() {
+        return basicSpacesUsed * crewRequiredPerSpace;
+    }
+
+    final int getDuelCost() {
+        return getEnhancedCost();
+    }
+
+    final int getEconomicCost() {
+        return Double.valueOf(Math.ceil(getDuelCost() * getProductionLevel().getEconomicCostModifier())).intValue();
+    }
+
+    final double getMaintenanceCostPerYear() {
+        return getEconomicCost() * getMaintenanceRate();
+    }
+
+    enum ProductionLevel {
+        PROTOTYPE(4d),
+        LIMITED(2d),
+        STANDARD(1d),
+        MASS(0.5d);
+
+        private final double economicCostModifier;
+
+        ProductionLevel(double economicCostModifier) {
+            this.economicCostModifier = economicCostModifier;
+        }
+
+        double getEconomicCostModifier() {
+            return economicCostModifier;
+        }
+    }
+}
---
+++ shipdesigner/src/test/java/com/senorpez/avt/shipdesigner/systems/HullTest.java
@@ -0,0 +1,132 @@
+package com.senorpez.avt.shipdesigner.systems;
+
+import com.senorpez.avt.shipdesigner.Ship;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class HullTest {
+    @Mock
+    Ship ship;
+
+    private Hull instance;
+    private final double tolerance = 0.001d;
+
+    @BeforeEach
+    void setUp() {
+        instance = new Hull(ship, 0, Hull.ProductionLevel.STANDARD);
+    }
+
+    @Test
+    void getName() {
+        String expectedValue = "Hull";
+        assertEquals(expectedValue, instance.getName());
+    }
+
+    @Test
+    void getSpacesPerSystem() {
+        int expectedValue = 0;
+        assertEquals(expectedValue, instance.getSpacesPerSystem());
+    }
+
+    @Test
+    void getBasicSpacesUsed() {
+        int expectedValue = 0;
+        assertEquals(expectedValue, instance.getBasicSpacesUsed());
+    }
+
+    @Test
+    void getActualSpacesUsed() {
+        int expectedValue = 0;
+        assertEquals(expectedValue, instance.getActualSpacesUsed());
+    }
+
+    @Test
+    void getCostPerSpace() {
+        int expectedValue = 1;
+        assertEquals(expectedValue, instance.getCostPerSpace());
+    }
+
+    @Test
+    void getCrewRequiredPerSpace() {
+        int expectedValue = 0;
+        assertEquals(expectedValue, instance.getCrewRequiredPerSpace());
+    }
+
+    @Test
+    void getProductionLevel() {
+        Hull.ProductionLevel expectedValue = Hull.ProductionLevel.STANDARD;
+        assertEquals(expectedValue, instance.getProductionLevel());
+    }
+
+    @Test
+    void getMaintenanceRate() {
+        double expectedValue = 0.15d;
+        assertEquals(expectedValue, instance.getMaintenanceRate());
+    }
+
+    @Test
+    void getBaseCost() {
+        when(ship.getHullSize()).thenReturn(55);
+        int expectedValue = 55;
+        assertEquals(expectedValue, instance.getBaseCost());
+    }
+
+    @Test
+    void getEnhancedCost() {
+        when(ship.getHullSize()).thenReturn(55);
+
+        instance.setShrinkEnhancement(0);
+        int expectedValue = 55;
+        assertEquals(expectedValue, instance.getEnhancedCost());
+
+        instance.setShrinkEnhancement(3);
+        expectedValue = 82;
+        assertEquals(expectedValue, instance.getEnhancedCost());
+    }
+
+    @Test
+    void getCrewRequired() {
+        int expectedValue = 0;
+        assertEquals(expectedValue, instance.getCrewRequired());
+    }
+
+    @Test
+    void getDuelCost() {
+        when(ship.getHullSize()).thenReturn(55);
+        int expectedValue = 55;
+        assertEquals(expectedValue, instance.getDuelCost());
+    }
+
+    @Test
+    void getEconomicCost() {
+        when(ship.getHullSize()).thenReturn(55);
+
+        instance.setProductionLevel(Hull.ProductionLevel.STANDARD);
+        int expectedValue = 55;
+        assertEquals(expectedValue, instance.getEconomicCost());
+
+        instance.setProductionLevel(Hull.ProductionLevel.PROTOTYPE);
+        expectedValue = 220;
+        assertEquals(expectedValue, instance.getEconomicCost());
+    }
+
+    @Test
+    void getMaintenanceCostPerYear() {
+        when(ship.getHullSize()).thenReturn(55);
+
+        instance.setProductionLevel(Hull.ProductionLevel.STANDARD);
+        double expectedValue = 8.25000d;
+        assertEquals(expectedValue, instance.getMaintenanceCostPerYear(), tolerance);
+
+        instance.setProductionLevel(Hull.ProductionLevel.PROTOTYPE);
+        expectedValue = 33.00000d;
+        assertEquals(expectedValue, instance.getMaintenanceCostPerYear(), tolerance);
+    }
+}
