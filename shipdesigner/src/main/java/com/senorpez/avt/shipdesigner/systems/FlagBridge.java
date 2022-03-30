00b0cb4d886410746bdd467460e16f65103df802
SenorPez <senorpez@senorpez.com>
2022-03-30 09:13:38 -0500

    Created FlagBridge

---
+++ shipdesigner/src/main/java/com/senorpez/avt/shipdesigner/systems/FlagBridge.java
@@ -0,0 +1,57 @@
+package com.senorpez.avt.shipdesigner.systems;
+
+import com.senorpez.avt.shipdesigner.Ship;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FlagBridge extends ArmoredSystem {
+    private final static String name = "Flag Bridge";
+    private final static int spacesPerSystem = 1;
+    private final static double costPerSpace = 3d;
+    private final static double crewRequiredPerSpace = 1d;
+    private final static double maintenanceRate = 0.25d;
+
+    FlagBridge(Ship ship,
+           int quantity,
+           int shrinkEnhancement,
+           ProductionLevel productionLevel,
+           int armorLevel) {
+        super(ship,
+                name,
+                spacesPerSystem,
+                costPerSpace,
+                crewRequiredPerSpace,
+                maintenanceRate,
+                quantity,
+                shrinkEnhancement,
+                productionLevel,
+                armorLevel);
+    }
+
+    List<Integer> getFlagPointsAt() {
+        final int hullSpaces = getShip().getHullSize();
+        if (hullSpaces < 100) return null;
+
+        List<Integer> val = new ArrayList<>();
+        val.add(4);
+
+        if (hullSpaces >= 150) val.add(6);
+        if (hullSpaces >= 200) val.add(8);
+        if (hullSpaces >= 250) val.add(10);
+        if (hullSpaces >= 300) val.add(12);
+        if (hullSpaces >= 350) val.add(14);
+        if (hullSpaces >= 400) val.add(16);
+        if (hullSpaces >= 450) val.add(18);
+
+        return val;
+    }
+
+    int getFlagPoints() {
+        if (getQuantity() >= 4) {
+            return Double.valueOf(Math.floor(1 + (getQuantity() - 4) / 2d)).intValue();
+        } else {
+            return 0;
+        }
+    }
+}
---
+++ shipdesigner/src/test/java/com/senorpez/avt/shipdesigner/systems/FlagBridgeTest.java
@@ -0,0 +1,158 @@
+package com.senorpez.avt.shipdesigner.systems;
+
+import com.senorpez.avt.shipdesigner.Ship;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class FlagBridgeTest {
+    @Mock
+    Ship ship;
+
+    private FlagBridge instance;
+    private final double tolerance = 0.001d;
+
+    @BeforeEach
+    void setUp() {
+        instance = new FlagBridge(
+                ship,
+                7,
+                6,
+                ProductionLevel.LIMITED,
+                3
+        );
+    }
+
+    @Test
+    void getName() {
+        String expectedValue = "Flag Bridge";
+        assertEquals(expectedValue, instance.getName());
+    }
+
+    @Test
+    void getQuantity() {
+        int expectedValue = 7;
+        assertEquals(expectedValue, instance.getQuantity());
+    }
+
+    @Test
+    void getFlagPointsAt() {
+        when(ship.getHullSize()).thenReturn(50);
+        assertNull(instance.getFlagPointsAt());
+
+        when(ship.getHullSize()).thenReturn(500);
+        List<Integer> expectedValue = new ArrayList<>(List.of(4, 6, 8, 10, 12, 14, 16, 18));
+        assertEquals(expectedValue, instance.getFlagPointsAt());
+    }
+
+    @Test
+    void getFlagPoints() {
+        int expectedValue = 2;
+        assertEquals(expectedValue, instance.getFlagPoints());
+    }
+
+    @Test
+    void getBasicSpacesUsed() {
+        int expectedValue = 7;
+        assertEquals(expectedValue, instance.getBasicSpacesUsed());
+    }
+
+    @Test
+    void getShrinkEnhancement() {
+        int expectedValue = 6;
+        assertEquals(expectedValue, instance.getShrinkEnhancement());
+    }
+
+    @Test
+    void getSpacesPerSystem() {
+        int expectedValue = 1;
+        assertEquals(expectedValue, instance.getSpacesPerSystem());
+    }
+
+    @Test
+    void getCostPerSpace() {
+        double expectedValue = 3d;
+        assertEquals(expectedValue, instance.getCostPerSpace(), tolerance);
+    }
+
+    @Test
+    void getCrewRequiredPerSpace() {
+        double expectedValue = 1d;
+        assertEquals(expectedValue, instance.getCrewRequiredPerSpace(), tolerance);
+    }
+
+    @Test
+    void getActualSpacesUsed() {
+        int expectedValue = 4;
+        assertEquals(expectedValue, instance.getActualSpacesUsed());
+    }
+
+    @Test
+    void getBaseCost() {
+        int expectedValue = 21;
+        assertEquals(expectedValue, instance.getBaseCost());
+    }
+
+    @Test
+    void getEnhancedCost() {
+        int expectedValue = 55;
+        assertEquals(expectedValue, instance.getEnhancedCost());
+    }
+
+    @Test
+    void getCrewRequirement() {
+        double expectedValue = 7d;
+        assertEquals(expectedValue, instance.getCrewRequirement(), tolerance);
+    }
+
+    @Test
+    void getArmorLevel() {
+        int expectedValue = 3;
+        assertEquals(expectedValue, instance.getArmorLevel());
+    }
+
+    @Test
+    void getArmorPointsUsed() {
+        double expectedValue = 8.00000d;
+        assertEquals(expectedValue, instance.getArmorPointsUsed(), tolerance);
+    }
+
+    @Test
+    void getDuelCost() {
+        int expectedValue = 55;
+        assertEquals(expectedValue, instance.getDuelCost());
+    }
+
+    @Test
+    void getProductionLevel() {
+        ProductionLevel expectedValue = ProductionLevel.LIMITED;
+        assertEquals(expectedValue, instance.getProductionLevel());
+    }
+
+    @Test
+    void getEconomicCost() {
+        int expectedValue = 110;
+        assertEquals(expectedValue, instance.getEconomicCost());
+    }
+
+    @Test
+    void getMaintenanceRate() {
+        double expectedValue = 0.25d;
+        assertEquals(expectedValue, instance.getMaintenanceRate(), tolerance);
+    }
+
+    @Test
+    void getMaintenanceCostPerYear() {
+        double expectedValue = 27.5d;
+        assertEquals(expectedValue, instance.getMaintenanceCostPerYear(), tolerance);
+    }
+}
