8e3e59e68edb8714b85fd96672ac20980a6d7a94
SenorPez <senorpez@senorpez.com>
2022-05-01 22:13:51 -0500

    Implemented ship hull spaces

---
+++ shipdesignernew/src/test/java/com/senorpez/avt/shipdesigner/ShipTest.java
@@ -0,0 +1,26 @@
+package com.senorpez.avt.shipdesigner;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ShipTest {
+    Ship instance;
+
+    @BeforeEach
+    void setUp() {
+        instance = new Ship();
+    }
+
+    @Test
+    void getHullSpaces() {
+        // Trivial test, but using to set up Github Actions.
+        instance = instance
+                .setHullSpaces(25)
+                .build();
+
+        int expectedValue = 25;
+        assertEquals(expectedValue, instance.getHullSpaces());
+    }
+}
