b6e9486cb9e6c927078f7f09e80862f8da1dcaad
SenorPez <senorpez@senorpez.com>
2022-07-08 10:38:10 -0500

    Implemented Structural Systems object

---
+++ ship-designer/src/main/java/com/senorpez/avt/shipdesigner/systems/StructuralSystems.java
@@ -0,0 +1,168 @@
+package com.senorpez.avt.shipdesigner.systems;
+
+import com.senorpez.avt.shipdesigner.Ship;
+
+import java.util.List;
+import java.util.function.ToDoubleFunction;
+import java.util.function.ToIntFunction;
+
+public class StructuralSystems {
+    private Hull hull;
+    private Drive drive;
+    private FrameReinforcement frameReinforcement;
+    private DriveAndMastArmor driveAndMastArmor;
+    private ExternalArmor externalArmor;
+    private InternalArmor internalArmor;
+    private Accessways accessways;
+
+    final private List<System> systems;
+
+    StructuralSystems(final Ship ship) {
+        final ArmorShrink armorShrink = new ArmorShrink(0);
+        final ArmorProductionLevel armorProductionLevel = new ArmorProductionLevel(ProductionLevel.STANDARD);
+
+        this.hull = new Hull(ship, 0, ProductionLevel.STANDARD);
+        this.drive = new Drive(ship, 0, ProductionLevel.STANDARD, 0);
+        this.frameReinforcement = new FrameReinforcement(ship, 0, ProductionLevel.STANDARD);
+        this.driveAndMastArmor = new DriveAndMastArmor(ship, 0, 0, armorShrink, armorProductionLevel);
+        this.externalArmor = new ExternalArmor(ship, 0, armorShrink, armorProductionLevel);
+        this.internalArmor = new InternalArmor(ship, 0, armorShrink, armorProductionLevel);
+        this.accessways = new Accessways(ship, false);
+
+        this.systems = List.of(
+                this.hull,
+                this.drive,
+                this.frameReinforcement,
+                this.driveAndMastArmor,
+                this.externalArmor,
+                this.internalArmor,
+                this.accessways
+        );
+    }
+
+    StructuralSystems(final Hull hull,
+                      final Drive drive,
+                      final FrameReinforcement frameReinforcement,
+                      final DriveAndMastArmor driveAndMastArmor,
+                      final ExternalArmor externalArmor,
+                      final InternalArmor internalArmor,
+                      final Accessways accessways) {
+        this.hull = hull;
+        this.drive = drive;
+        this.frameReinforcement = frameReinforcement;
+        this.driveAndMastArmor = driveAndMastArmor;
+        this.externalArmor = externalArmor;
+        this.internalArmor = internalArmor;
+        this.accessways = accessways;
+
+        this.systems = List.of(
+                this.hull,
+                this.drive,
+                this.frameReinforcement,
+                this.driveAndMastArmor,
+                this.externalArmor,
+                this.internalArmor,
+                this.accessways
+        );
+    }
+
+    int getBasicSpacesUsed() {
+        return systemsSum(System::getBasicSpacesUsed);
+    }
+
+    int getActualSpacesUsed() {
+        return systemsSum(System::getActualSpacesUsed);
+    }
+
+    int getBaseCost() {
+        return systemsSum(System::getBaseCost);
+    }
+
+    int getEnhancedCost() {
+        return systemsSum(System::getEnhancedCost);
+    }
+
+    int getArmorPointsUsed() {
+        return 0;
+    }
+
+    int getDuelCost() {
+        return systemsSum(System::getDuelCost);
+    }
+
+    int getEconomicCost() {
+        return systemsSum(System::getEconomicCost);
+    }
+
+    double getMaintenanceCost() {
+        return systemsSum(System::getMaintenanceCost);
+    }
+
+    private int systemsSum(final ToIntFunction<System> f) {
+        return getSystems().stream().mapToInt(f).sum();
+    }
+
+    private double systemsSum(final ToDoubleFunction<System> f) {
+        return getSystems().stream().mapToDouble(f).sum();
+    }
+
+    Hull getHull() {
+        return hull;
+    }
+
+    void setHull(final Hull hull) {
+        this.hull = hull;
+    }
+
+    Drive getDrive() {
+        return drive;
+    }
+
+    void setDrive(final Drive drive) {
+        this.drive = drive;
+    }
+
+    FrameReinforcement getFrameReinforcement() {
+        return frameReinforcement;
+    }
+
+    void setFrameReinforcement(final FrameReinforcement frameReinforcement) {
+        this.frameReinforcement = frameReinforcement;
+    }
+
+    DriveAndMastArmor getDriveAndMastArmor() {
+        return driveAndMastArmor;
+    }
+
+    void setDriveAndMastArmor(final DriveAndMastArmor driveAndMastArmor) {
+        this.driveAndMastArmor = driveAndMastArmor;
+    }
+
+    ExternalArmor getExternalArmor() {
+        return externalArmor;
+    }
+
+    void setExternalArmor(final ExternalArmor externalArmor) {
+        this.externalArmor = externalArmor;
+    }
+
+    InternalArmor getInternalArmor() {
+        return internalArmor;
+    }
+
+    void setInternalArmor(final InternalArmor internalArmor) {
+        this.internalArmor = internalArmor;
+    }
+
+    Accessways getAccessways() {
+        return accessways;
+    }
+
+    void setAccessways(final Accessways accessways) {
+        this.accessways = accessways;
+    }
+
+    private List<System> getSystems() {
+        return systems;
+    }
+}
---
+++ ship-designer/src/test/java/com/senorpez/avt/shipdesigner/systems/StructuralSystemsTest.java
@@ -0,0 +1,187 @@
+package com.senorpez.avt.shipdesigner.systems;
+
+import com.senorpez.avt.shipdesigner.Ship;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static com.senorpez.avt.shipdesigner.HullShape.SPHERE;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class StructuralSystemsTest {
+    @Mock
+    Ship ship;
+    @Mock
+    Hull hull;
+    @Mock
+    Drive drive;
+    @Mock
+    FrameReinforcement frameReinforcement;
+    @Mock
+    DriveAndMastArmor driveAndMastArmor;
+    @Mock
+    ExternalArmor externalArmor;
+    @Mock
+    InternalArmor internalArmor;
+    @Mock
+    Accessways accessways;
+
+    private StructuralSystems instance;
+
+    @Test
+    void initialShipTest() {
+        // Mock out as if a blank ship.
+        when(hull.getBasicSpacesUsed()).thenReturn(0);
+        when(hull.getActualSpacesUsed()).thenReturn(0);
+        when(hull.getBaseCost()).thenReturn(25);
+        when(hull.getEnhancedCost()).thenReturn(25);
+        when(hull.getDuelCost()).thenReturn(25);
+        when(hull.getEconomicCost()).thenReturn(25);
+        when(hull.getMaintenanceCost()).thenReturn(3.75d);
+
+        when(drive.getBasicSpacesUsed()).thenReturn(1);
+        when(drive.getActualSpacesUsed()).thenReturn(1);
+        when(drive.getBaseCost()).thenReturn(5);
+        when(drive.getEnhancedCost()).thenReturn(5);
+        when(drive.getDuelCost()).thenReturn(5);
+        when(drive.getEconomicCost()).thenReturn(5);
+        when(drive.getMaintenanceCost()).thenReturn(1d);
+
+        when(frameReinforcement.getBasicSpacesUsed()).thenReturn(0);
+        when(frameReinforcement.getActualSpacesUsed()).thenReturn(0);
+        when(frameReinforcement.getBaseCost()).thenReturn(0);
+        when(frameReinforcement.getEnhancedCost()).thenReturn(0);
+        when(frameReinforcement.getDuelCost()).thenReturn(0);
+        when(frameReinforcement.getEconomicCost()).thenReturn(0);
+        when(frameReinforcement.getMaintenanceCost()).thenReturn(0d);
+
+        when(driveAndMastArmor.getBasicSpacesUsed()).thenReturn(0);
+        when(driveAndMastArmor.getActualSpacesUsed()).thenReturn(0);
+        when(driveAndMastArmor.getBaseCost()).thenReturn(0);
+        when(driveAndMastArmor.getEnhancedCost()).thenReturn(0);
+        when(driveAndMastArmor.getDuelCost()).thenReturn(0);
+        when(driveAndMastArmor.getEconomicCost()).thenReturn(0);
+        when(driveAndMastArmor.getMaintenanceCost()).thenReturn(0d);
+
+        when(externalArmor.getBasicSpacesUsed()).thenReturn(0);
+        when(externalArmor.getActualSpacesUsed()).thenReturn(0);
+        when(externalArmor.getBaseCost()).thenReturn(0);
+        when(externalArmor.getEnhancedCost()).thenReturn(0);
+        when(externalArmor.getDuelCost()).thenReturn(0);
+        when(externalArmor.getEconomicCost()).thenReturn(0);
+        when(externalArmor.getMaintenanceCost()).thenReturn(0d);
+
+        when(internalArmor.getBasicSpacesUsed()).thenReturn(0);
+        when(internalArmor.getActualSpacesUsed()).thenReturn(0);
+        when(internalArmor.getBaseCost()).thenReturn(0);
+        when(internalArmor.getEnhancedCost()).thenReturn(0);
+        when(internalArmor.getDuelCost()).thenReturn(0);
+        when(internalArmor.getEconomicCost()).thenReturn(0);
+        when(internalArmor.getMaintenanceCost()).thenReturn(0d);
+
+        when(accessways.getBasicSpacesUsed()).thenReturn(0);
+        when(accessways.getActualSpacesUsed()).thenReturn(0);
+        when(accessways.getBaseCost()).thenReturn(0);
+        when(accessways.getEnhancedCost()).thenReturn(0);
+        when(accessways.getDuelCost()).thenReturn(0);
+        when(accessways.getEconomicCost()).thenReturn(0);
+        when(accessways.getMaintenanceCost()).thenReturn(0d);
+
+        instance = new StructuralSystems(hull,
+                drive,
+                frameReinforcement,
+                driveAndMastArmor,
+                externalArmor,
+                internalArmor,
+                accessways);
+
+        assertEquals(1, instance.getBasicSpacesUsed());
+        assertEquals(1, instance.getActualSpacesUsed());
+        assertEquals(30, instance.getBaseCost());
+        assertEquals(30, instance.getEnhancedCost());
+        assertEquals(0, instance.getArmorPointsUsed());
+        assertEquals(30, instance.getDuelCost());
+        assertEquals(30, instance.getEconomicCost());
+        assertEquals(4.75d, instance.getMaintenanceCost());
+    }
+
+    @Test
+    void yorkTest() {
+        // Mock out as if a York.
+        when(hull.getBasicSpacesUsed()).thenReturn(0);
+        when(hull.getActualSpacesUsed()).thenReturn(0);
+        when(hull.getBaseCost()).thenReturn(25);
+        when(hull.getEnhancedCost()).thenReturn(25);
+        when(hull.getDuelCost()).thenReturn(25);
+        when(hull.getEconomicCost()).thenReturn(25);
+        when(hull.getMaintenanceCost()).thenReturn(3.75d);
+
+        when(drive.getBasicSpacesUsed()).thenReturn(4);
+        when(drive.getActualSpacesUsed()).thenReturn(4);
+        when(drive.getBaseCost()).thenReturn(79);
+        when(drive.getEnhancedCost()).thenReturn(79);
+        when(drive.getDuelCost()).thenReturn(79);
+        when(drive.getEconomicCost()).thenReturn(79);
+        when(drive.getMaintenanceCost()).thenReturn(15.8d);
+
+        when(frameReinforcement.getBasicSpacesUsed()).thenReturn(0);
+        when(frameReinforcement.getActualSpacesUsed()).thenReturn(0);
+        when(frameReinforcement.getBaseCost()).thenReturn(0);
+        when(frameReinforcement.getEnhancedCost()).thenReturn(0);
+        when(frameReinforcement.getDuelCost()).thenReturn(0);
+        when(frameReinforcement.getEconomicCost()).thenReturn(0);
+        when(frameReinforcement.getMaintenanceCost()).thenReturn(0d);
+
+        when(driveAndMastArmor.getBasicSpacesUsed()).thenReturn(0);
+        when(driveAndMastArmor.getActualSpacesUsed()).thenReturn(0);
+        when(driveAndMastArmor.getBaseCost()).thenReturn(0);
+        when(driveAndMastArmor.getEnhancedCost()).thenReturn(0);
+        when(driveAndMastArmor.getDuelCost()).thenReturn(0);
+        when(driveAndMastArmor.getEconomicCost()).thenReturn(0);
+        when(driveAndMastArmor.getMaintenanceCost()).thenReturn(0d);
+
+        when(externalArmor.getBasicSpacesUsed()).thenReturn(0);
+        when(externalArmor.getActualSpacesUsed()).thenReturn(0);
+        when(externalArmor.getBaseCost()).thenReturn(0);
+        when(externalArmor.getEnhancedCost()).thenReturn(0);
+        when(externalArmor.getDuelCost()).thenReturn(0);
+        when(externalArmor.getEconomicCost()).thenReturn(0);
+        when(externalArmor.getMaintenanceCost()).thenReturn(0d);
+
+        when(internalArmor.getBasicSpacesUsed()).thenReturn(4);
+        when(internalArmor.getActualSpacesUsed()).thenReturn(4);
+        when(internalArmor.getBaseCost()).thenReturn(8);
+        when(internalArmor.getEnhancedCost()).thenReturn(8);
+        when(internalArmor.getDuelCost()).thenReturn(8);
+        when(internalArmor.getEconomicCost()).thenReturn(8);
+        when(internalArmor.getMaintenanceCost()).thenReturn(0.4d);
+
+        when(accessways.getBasicSpacesUsed()).thenReturn(0);
+        when(accessways.getActualSpacesUsed()).thenReturn(0);
+        when(accessways.getBaseCost()).thenReturn(0);
+        when(accessways.getEnhancedCost()).thenReturn(0);
+        when(accessways.getDuelCost()).thenReturn(0);
+        when(accessways.getEconomicCost()).thenReturn(0);
+        when(accessways.getMaintenanceCost()).thenReturn(0d);
+
+        instance = new StructuralSystems(hull,
+                drive,
+                frameReinforcement,
+                driveAndMastArmor,
+                externalArmor,
+                internalArmor,
+                accessways);
+
+        assertEquals(8, instance.getBasicSpacesUsed());
+        assertEquals(8, instance.getActualSpacesUsed());
+        assertEquals(112, instance.getBaseCost());
+        assertEquals(112, instance.getEnhancedCost());
+        assertEquals(0, instance.getArmorPointsUsed());
+        assertEquals(112, instance.getDuelCost());
+        assertEquals(112, instance.getEconomicCost());
+        assertEquals(19.95d, instance.getMaintenanceCost());
+    }
+}
